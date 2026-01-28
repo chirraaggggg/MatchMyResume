53d2ff024fc8da98c18100304e32ffd691701360
Chirag <chhirag920@gmail.com>
2026-01-28 08:50:39 +0530

    pushing the code

---
+++ app/routes/auth.tsx
@@ -0,0 +1,14 @@
+export const meta:  () => ({title: string; name?: undefined;... = () => ([
+    { title: 'MatchMyResume | Auth' },
+    { name: 'description', content: 'Log into your account' }
+])})
+
+const auth = () => {
+  return (
+    <div>
+      auth page
+    </div>
+  )
+}
+
+export default auth
