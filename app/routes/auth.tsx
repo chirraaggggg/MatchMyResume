3bf4aaca836cae82c16556fb90d4eaf5ad938e4f
Chirag <chhirag920@gmail.com>
2026-01-28 17:51:59 +0530

    adding auth

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,13 +1,54 @@
-export const meta:  () => ({title: string; name?: undefined;... = () => ([
+import { usePuterStore } from "~/lib/puter";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+
+
+export const meta = () => ([
     { title: 'MatchMyResume | Auth' },
     { name: 'description', content: 'Log into your account' }
-])})
+])
 
 const auth = () => {
+    const { isLoading, auth } = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(auth.isAuthenticated) navigate(next);
+    }, [auth.isAuthenticated, next])
   return (
-    <div>
-      auth page
-    </div>
+    <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+        <div className="gradient-border shadow-lg p-8 rounded-lg">
+            <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                <div className="flex flex-col items-center gap-2 text-center">
+                    <h1>Welcome</h1>
+                    <h2>Log into your account</h2>
+                </div>
+                <div>
+                    {isLoading ? (
+                        <button className="auth-button animate-pulse">
+                            <p>Signing you in...</p>
+                        </button>
+                    ) : (
+                        <>
+                            {auth.isAuthenticated ? (
+                                <button
+                                    className="auth-button" onClick={auth.signOut}>
+                                        <p>Log Out</p>
+                                </button>
+                            ) : (
+                                <button
+                                    className="auth-button" onClick={auth.signIn}>
+                                        <p>Sign In</p>
+                                </button>
+                            )}
+                        </>
+                    )}
+                </div>
+            </section>
+        </div>
+    </main>
   )
 }
 
